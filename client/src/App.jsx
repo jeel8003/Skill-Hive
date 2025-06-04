535a1e9c98947375e2faf2438da912f2ed8e12ed
jeel8003 <jeelboghra354@gmail.com>
2025-06-04 15:45:24 +0530

    2nd commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
 import Login from './pages/Login'
+import Navbar from './components/navbar'
+import HeroSection from './pages/student/HeroSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
+      <Navbar/>
+      <HeroSection/>
+      
       <Login/>
     </main>
   )
