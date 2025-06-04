dfe3ddd2e54151560aeec1607f4d0109c9b04f08
jeel8003 <jeelboghra354@gmail.com>
2025-06-04 16:50:04 +0530

    commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { appStore } from './app/store'
-import { Toaster } from 'sonner'
-// import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { appStore } from "./app/store";
+import { Toaster } from "./components/ui/sonner";
+import { useLoadUserQuery } from "./features/api/authApi";
+import LoadingSpinner from "./components/LoadingSpinner";
 
-createRoot(document.getElementById('root')).render(
+const Custom = ({ children }) => {
+  const { isLoading } = useLoadUserQuery();
+  return <>{isLoading ? <LoadingSpinner/> : <>{children}</>}</>;
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={appStore}>
-    <App />
-    <Toaster/>
+      <Custom>
+        <App />
+        <Toaster />
+      </Custom>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
